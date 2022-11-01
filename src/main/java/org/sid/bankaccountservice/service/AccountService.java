01a338d8f3810f0e2a904921e1cefd0f8701bc77
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-11-02 00:35:13 +0100

    GraphQL

--- src/main/java/org/sid/bankaccountservice/service/AccountService.java
+++ src/main/java/org/sid/bankaccountservice/service/AccountService.java
@@ -5,4 +5,6 @@ import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
 
 public interface AccountService {
     BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+
+    BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO bankAccountDTO);
 }
